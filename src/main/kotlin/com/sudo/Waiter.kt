31628ed64fdb3e6a088e748eb10451b2aaafe13a
eugenpolytechnic <zenya.01@mail.ru>
2020-09-19 12:47:15 +0300

    first code

---
+++ src/main/kotlin/com/sudo/Waiter.kt
@@ -0,0 +1,14 @@
+package com.sudo
+
+data class Waiter(
+        val q1: String,
+        val q2: Boolean,
+        val q3: String,
+        val q4: Int,
+        val q5: Boolean,
+        val q6: List<String>,
+        val q7: List<String>,
+        val q8: Map<String, String>,
+        val q9: List<String>,
+        val q10: List<Int>,
+)
